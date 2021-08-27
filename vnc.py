77843ed96c0ef719c5776f3fe047c5281f9437af
trinity 341 <89635125+alexdanca341341@users.noreply.github.com>
2021-08-27 13:45:49 +0300

    Add files via upload

---
+++ vnc.py
@@ -0,0 +1,13 @@
+import os
+
+cmd = 'sudo apt update'
+os.system(cmd)
+cmd = 'sudo apt-get install --assume-yes ./chrome-remote-desktop_current_amd64.deb'
+os.system(cmd)
+cmd = 'sudo apt install --assume-yes xfce4 desktop-base dbus-x11 xscreensaver'
+os.system(cmd)
+cmd = 'sudo apt-get install geany -y'
+os.system(cmd)
+cmd = 'sudo apt-get install vim-gtk3 -y'
+os.system(cmd)
+print("vnc up")
